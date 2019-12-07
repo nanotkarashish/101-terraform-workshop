7afd7e418054d43017c3c9cc7b677ee376e9548e
ananotkar <ananotkar@equalexperts.com>
2019-12-07 16:13:12 +0530

    completing step-3

--- workshop/ec2-instance.tf
+++ workshop/ec2-instance.tf
@@ -1,18 +1,7 @@
-variable "sshkeypath" {
-  default = ""
-}
-
-resource "aws_key_pair" "ssh_key" {
-  key_name = "godkey"
-  public_key = var.ssh_key
-}
-
 resource "aws_instance" "first_instance" {
   ami = "ami-04b9e92b5572fa0d1"
 
   subnet_id = "subnet-302b7d79"
-
-  key_name = aws_key_pair.ssh_key.key_name
 
   instance_type = "t2.micro"
 
--- workshop/provider.tf
+++ workshop/provider.tf
@@ -2,15 +2,13 @@ terraform {
   required_version = "0.12.16"
 
   backend "s3" {
-    bucket = ""
-    region = ""
-    key = "/terraform.tfstate"
+    bucket = "ashishterra"
+    region = "us-east-1"
+    key = "workshop/terraform.tfstate"
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.secret_key
 }
 
